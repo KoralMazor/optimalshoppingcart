a94b5c1b08bccbe0b7c926941c9d97777d792787
D0rb <dor.bismut@gmail.com>
2021-05-31 13:07:39 +0300

    Updated to DataModel and added a CTOR

--- src/main/java/utilities/CartObject.java
+++ src/main/java/utilities/CartObject.java
@@ -1,8 +1,26 @@
 package utilities;
 
 public class CartObject {
-        public String Name;
-        public String Type;
-        public double Price;
-        public double Weight;
+        String Name;
+        String Type;
+        double Price;
+        double Weight;
+
+       public CartObject(String name ,String type , double price,double weight){
+               this.Name = name;
+               this.Type = type;
+               this.Price = price;
+               this.Weight = weight;
+     }
+     public void setName(String name){ this.Name = name; }
+     public void setType(String type){ this.Type = type; }
+     public void setPrice(double price){ this.Price = price; }
+     public void setWeight(double weight){ this.Weight = weight; }
+
+     public String getName(){ return this.Name; }
+     public String getType(){ return this.Type; }
+     public double getPrice(){ return this.Price; }
+     public double getWeight(){ return this.Weight; }
+
+
 }
